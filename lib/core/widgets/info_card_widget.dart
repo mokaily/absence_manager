7370ee04079fb907afb6910b9c929acf31915234
Mohamed Okaily <mosala7.me@gmail.com>
2026-02-08 00:58:53 +0100

    Create info_card_widget.dart

---
+++ lib/core/widgets/info_card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class InfoCardWidget extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final IconData icon;
+  const InfoCardWidget({super.key, required this.title, required this.subTitle, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).brightness == Brightness.dark ? Color(0xFF1e2936) : Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Theme.of(context).brightness == Brightness.dark ? Color(0xFF374151) : Color(0xFFE5E7EB),
+        ),
+        boxShadow: [
+          BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 4, offset: const Offset(0, 2)),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyle(fontSize: 18, color: Colors.grey.shade600, fontWeight: FontWeight.w500),
+              ),
+              Icon(icon, size: 16, color: Colors.blueGrey),
+            ],
+          ),
+          SizedBox(height: 24),
+          Text(
+            subTitle,
+            style: TextStyle(fontSize: 14, color: Colors.grey.shade600, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+}
